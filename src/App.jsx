b06b94d44b8908a55d4905a4d76b47e083260c67
Fadilla Ma'sum Haqqoni <fadillamaksum650@gmail.com>
2025-05-18 14:11:54 +0700

    nambah Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  function handleLogin() {
+    setIsLogin(!isLogin);
+  }
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center mx-auto">
-      <h2 className="text-2xl font-bold text-gray-800">Tailwind Card</h2>
-      <p className="text-gray-600 mt-3">This is a simple card layout built with Tailwind CSS.</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Learn More</button>
-    </div>
+    <>
+      <Navbar isLogin={isLogin} />
+      <button
+        onClick={handleLogin}
+        className="p-4 bg-blue-500 text-white rounded"
+      >
+        {" "}
+        Cek Login bang
+      </button>
+    </>
   );
 }
 
